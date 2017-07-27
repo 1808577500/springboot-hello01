a25544cc38721e58bdde5cf71b2334a92847bf51
zhangchx <zhangchx@sh-stt.com>
2017-07-27 15:08:23 +0800

    springboot-helloworld
    Change-Id: I5d7b1ea78a3b0d6bfde422e5b1ca2f6e698bb1c7

---
+++ springboot/src/main/java/com/stt/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.stt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:spring boot 启动类
+ */
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
